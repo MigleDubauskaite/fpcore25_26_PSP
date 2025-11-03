3d1f39294f32679820c3e80aefa0938919faedad
Migle DB <dubauskaitemigle@gmail.com>
2025-11-03 22:08:56 +0100

    practicando hilos con luciernagas y otros ejercicios

---
+++ src/fpcore25_26/preparacion_examen/luciernagas/adicional/LetraIluminada.java
@@ -0,0 +1,51 @@
+package fpcore25_26.preparacion_examen.luciernagas.adicional;
+
+public class LetraIluminada {
+
+	private static final String GREEN = "\u001B[32m";
+	private static final String RESET = "\u001B[0m";
+
+	private String palabra;
+	private static int milis = 400;
+
+	public LetraIluminada(String palabra) {
+		this.palabra = palabra;
+	}
+
+	private void iluminarLetra() {
+
+		char[] letras = palabra.toCharArray();
+
+		for (int i = 0; i < palabra.length(); i++) {
+			StringBuilder sb = new StringBuilder();
+
+			for (int j = 0; j < letras.length; j++) {
+				if (i == j) {
+					sb.append(GREEN + Character.toUpperCase(letras[j]) + RESET);
+				} else {
+					sb.append(Character.toLowerCase(letras[j]));
+				}
+			}
+			System.out.println(sb);
+
+			try {
+				Thread.sleep(milis);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		LetraIluminada li = new LetraIluminada("Migle");
+		li.iluminarLetra();
+
+		System.out.println();
+		LetraIluminada li2 = new LetraIluminada("Arbolito");
+		li2.iluminarLetra();
+
+	}
+
+}
--- src/fpcore25_26/preparacion_examen/luciernagas/adicional/RevelacionPalabra.java
+++ src/fpcore25_26/preparacion_examen/luciernagas/adicional/RevelacionPalabra.java
@@ -1,51 +1,42 @@
-package fpcore25_26.preparacion_examen.luciernagas;
+package fpcore25_26.preparacion_examen.luciernagas.adicional;
 
-public class Deletrator {
+public class RevelacionPalabra {
 
 	private String palabra;
 	private static int milis = 600;
 
-	public Deletrator(String palabra) {
+	public RevelacionPalabra(String palabra) {
 		this.palabra = palabra;
 	}
 
-	private void deletrear() {
+	private void revelarPalabra() {
 
-		char letras[] = palabra.toCharArray();
+		char[] letras = palabra.toCharArray();
 
 		for (int i = 0; i < palabra.length(); i++) {
 
 			StringBuilder sb = new StringBuilder();
 
 			for (int j = 0; j < letras.length; j++) {
-
-				if (j == i)
+				if (j <= i) {
 					sb.append(palabra.charAt(j));
-				else
+				} else
 					sb.append("-");
 			}
 
-			System.out.printf("Deletreando \"%s\": [%s] %n", palabra, sb);
+			System.out.println(sb);
 
 			try {
 				Thread.sleep(milis);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-
 		}
 	}
 
 	public static void main(String[] args) {
-
-		Deletrator d1 = new Deletrator("hola");
-		d1.deletrear();
-
-		System.out.println();
-
-		Deletrator d2 = new Deletrator("Migle Dubauskaite");
-		d2.deletrear();
-
+		RevelacionPalabra rp = new RevelacionPalabra("Holi");
+		rp.revelarPalabra();
 	}
 
 }
